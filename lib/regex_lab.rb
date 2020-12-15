dc1ba9bb18ed896b1e694b7a305f2c481d00a9ec
JC <bonassin@gmail.com>
2020-12-15 14:31:09 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,8 @@
 def starts_with_a_vowel?(word)
-  if word.match (/\b[aeiou]/) = true
+  if word.match (/\b[aeiou]/) == nil
+    return false
+  else
+    return true
   end
 
 def words_starting_with_un_and_ending_with_ing(text)
